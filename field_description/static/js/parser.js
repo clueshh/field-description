946ded523d3ccf975c140f074c3b557646e59bb7
clueshh <campbell.whitworth@gmail.com>
2019-02-02 16:33:06 +1300

    updates

--- field_description/static/js/parser.js
+++ field_description/static/js/parser.js
@@ -1,10 +1,13 @@
 // main parser ofr output string
 function parser() {
-    var subordinate_fraction = ''
-    var major_fraction = ''
-    var minor_fraction = ''
-    var color1 = ''
-    var color2 = ''
-    var color3 = ''
-    var structure = ''
+    var subordinate_fraction = $("#select_subordinate_fraction").find(":selected").text();
+
+    var major_fraction = $("#select_major_fraction").find(":selected").text();
+    var grain_size = $("#select_behaviour").find(":selected").text();
+    
+    var minor_fraction = $("#select_subordinate_fraction").find(":selected").text();
+    var color1 = $("#select_subordinate_fraction").find(":selected").text();
+    var color2 = $("#select_subordinate_fraction").find(":selected").text();
+    var color3 = $("#select_subordinate_fraction").find(":selected").text();
+    var structure = $("#select_subordinate_fraction").find(":selected").text();
 };

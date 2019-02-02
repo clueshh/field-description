09d4231fb538c81485066de29ac97d397ffe9c9c
clueshh <campbell.whitworth@gmail.com>
2019-02-02 16:09:42 +1300

    add files

---
+++ field_description/static/js/parser.js
@@ -0,0 +1,10 @@
+// main parser ofr output string
+function parser() {
+    var subordinate_fraction = ''
+    var major_fraction = ''
+    var minor_fraction = ''
+    var color1 = ''
+    var color2 = ''
+    var color3 = ''
+    var structure = ''
+};

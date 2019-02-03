f52ca7dc8963f17999a3ded722ea2d91d5aae3d9
clueshh <campbell.whitworth@gmail.com>
2019-02-03 16:11:41 +1300

    updates

--- field_description/__init__.py
+++ field_description/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 # Initiate Flask
 app = Flask(__name__)
-app.config.from_object('config.Config')
+app.config.from_object('config.ProductionConfig')
 
 import field_description.views
 
